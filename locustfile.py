aea460adeb5e15fa1bf694071b72819ee828c1c5
meemmahmuda <meemmahmuda85@gmail.com>
2025-07-30 16:42:27 +0600

    up

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,3 @@
-
 # from locust import HttpUser, task
 # from locust import HttpLocust, HttpUser, TaskSet, task
 
@@ -8,13 +7,38 @@
 #          self.client.get("https://list.sanjoydeyreju.com/supervisor/dashboard")
 
 
+# from locust import HttpUser, task, between
+
+# class HelloWorldUser(HttpUser):
+#     wait_time = between(1, 3)  
+#     @task
+#     def helloworld(self):
+#         self.client.get("/")
+
+import re
 from locust import HttpUser, task, between
 
-class HelloWorldUser(HttpUser):
-    wait_time = between(1, 3)  
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 3)
+
     @task
-    def helloworld(self):
-        self.client.get("/supervisor/dashboard")
+    def login(self):
+        # Step 1: GET login page
+        res = self.client.get("/", name="Get Login Page")
+
+        # Step 2: Extract CSRF token
+        token = re.search(r'name="_token"\s*value="(.+?)"', res.text)
+        if not token:
+            return
+
+        # Step 3: POST login
+        self.client.post("/admin_login", data={
+            "email": "admin@gmail.com",
+            "password": "111",
+            "_token": token.group(1)
+        }, name="Login")
+
+
 
 
         #  python -m locust
