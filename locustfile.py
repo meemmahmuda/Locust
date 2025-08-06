25d612a6958bdaa7ce6fcd2d71e9f83bf4f35034
meemmahmuda <meemmahmuda85@gmail.com>
2025-08-06 16:43:13 +0600

    up

--- locustfile.py
+++ locustfile.py
@@ -16,28 +16,28 @@
 #         self.client.get("/")
 
 
-import re
-from locust import HttpUser, task, between
+# import re
+# from locust import HttpUser, task, between
 
-class WebsiteUser(HttpUser):
-    wait_time = between(1, 3)
+# class WebsiteUser(HttpUser):
+#     wait_time = between(1, 3)
 
-    @task
-    def login(self):
-        # Step 1: GET login page
-        res = self.client.get("/", name="Get Login Page")
+#     @task
+#     def login(self):
+#         # Step 1: GET login page
+#         res = self.client.get("/", name="Get Login Page")
 
-        # Step 2: Extract CSRF token
-        token = re.search(r'name="_token"\s*value="(.+?)"', res.text)
-        if not token:
-            return
+#         # Step 2: Extract CSRF token
+#         token = re.search(r'name="_token"\s*value="(.+?)"', res.text)
+#         if not token:
+#             return
 
-        # Step 3: POST login
-        self.client.post("/admin_login", data={
-            "email": "admin@gmail.com",
-            "password": "111",
-            "_token": token.group(1)
-        }, name="Login")
+#         # Step 3: POST login
+#         self.client.post("/admin_login", data={
+#             "email": "admin@gmail.com",
+#             "password": "111",
+#             "_token": token.group(1)
+#         }, name="Login")
 
 
 
@@ -70,3 +70,30 @@ class WebsiteUser(HttpUser):
 #             "_token": otp_token.group(1),
 #             "otp": "123456"
 #         })
+
+
+import re
+from locust import HttpUser, task, between
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def login(self):
+        r = self.client.get("/deliveryman/login")
+        token = re.search(r'name="_token"\s*value="(.+?)"', r.text)
+        if not token:
+            return
+        login_res = self.client.post("/deliveryman/authenticate", data={
+            "_token": token.group(1),
+            "email": "dm1@gmail.com",
+            "password": "123456789"
+        })
+        r = self.client.get("/deliveryman/otp-verify")
+        otp_token = re.search(r'name="_token"\s*value="(.+?)"', r.text)
+        if not otp_token:
+            return
+        self.client.post("/deliveryman/otp-verify", data={
+            "_token": otp_token.group(1),
+            "otp": "123456"
+        })
